2c19112b1fd139582094dac01df9cd15f196c547
chenzhen <czhttp@gmail.com>
2016-12-30 15:03:30 +0800

    update dlopen

--- src/main.cc
+++ src/main.cc
@@ -4,10 +4,33 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <dlfcn.h>
 #include "main.h"
+
+typedef struct {
+ const char *module;
+ int (*GetValue)(char *pszVal);
+ int (*PrintfHello)();
+} so_API;
+
+
 
 int main(int argc, char *argv[]) {
   std::cout << "I am in main!" << std::endl;
   call_process(2); 
+
+  //dlopen model:
+  std::string so = "so/libtest.so";
+  void *so_obj = dlopen((char*)so.c_str(),RTLD_LAZY);
+  if (so_obj == NULL) {
+      std::cout << "so is not exist!" << std::endl;
+  }
+
+  FunPtr func = (FunPtr)dlsym(so_obj, "call_process");
+
+  if (func != NULL) {
+      (*func)(3);
+  }
+
   return 0;
 }
--- src/main.h
+++ src/main.h
@@ -2,7 +2,7 @@
  *  main.h  *
  ************/
 
-#ifndef MAIN
-#define MAIN value
+#ifndef MAIN_H_
+#define MAIN_H_ value
 extern "C" void call_process(int i);
 #endif
